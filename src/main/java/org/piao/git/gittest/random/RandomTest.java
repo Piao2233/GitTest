42daa4b6d78cddac6554b07f0a7d7754578f7b9e
ZhaoYing Cheng <2119828525@qq.com>
2023-01-01 16:19:37 +0800

    打印GitGitGitGit

---
+++ src/main/java/org/piao/git/gittest/random/RandomTest.java
@@ -0,0 +1,15 @@
+package org.piao.git.gittest.random;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author：piao
+ * @Date：2023/1/1 4:18 PM
+ */
+@Slf4j
+public class RandomTest {
+
+    public static void main(String[] args) {
+        log.info("GitGitGitGitGitGit");
+    }
+}
