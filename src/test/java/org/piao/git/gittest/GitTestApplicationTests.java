606dec3ff32d3a754818c223739ed2030253d9aa
ZhaoYing Cheng <2119828525@qq.com>
2023-01-01 16:16:59 +0800

    gittest的第一次提交

---
+++ src/test/java/org/piao/git/gittest/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piao.git.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
